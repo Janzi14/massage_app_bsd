877109668393b33c50530170dd240d31df369c9b
grabnerjulia13 <s51169@edu.campus02.at>
2023-11-20 16:34:25 +0100

    add endpoint to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { TreatmentsComponent } from './views/treatments/treatments.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { TeamComponent } from './views/team/team.component';
+import {provideHttpClient} from "@angular/common/http";
 
 @NgModule({
   imports: [
     BrowserModule,
     NgbModule
   ],
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/views/team/team.component.ts
+++ src/app/views/team/team.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css'],
+  imports: [NgForOf],
   standalone: true
 })
-export class TeamComponent {
+
+export class TeamComponent{
+  constructor(private http: HttpClient) { }
+
+  teammembers?: TeamMember[];
+
+  ngOnInit():void {
+    this.http.get<TeamMember[]>('http://localhost:3000/team').subscribe(data => {
+      console.log(data);
+      this.teammembers=data;}
+    );
+  }
 
 }
