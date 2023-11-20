3fd2b792acb69f37ac812b6f4b592461a8a5ee33
Jakob Janz <j.janz@citiesapps.com>
2023-11-20 08:01:54 +0100

    make components standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports:[HeaderComponent],
+  standalone: true
 })
 export class AppComponent {
   title = 'massageApp';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
   imports: [
     BrowserModule,
     NgbModule
