363336abc16dea349123415811379ca9bb9e0ba5
Jakob Janz <j.janz@citiesapps.com>
2023-11-21 10:23:55 +0100

    add modify and delete treatment buttons

--- src/app/types/treatments.d.ts
+++ src/app/types/treatments.d.ts
@@ -2,7 +2,7 @@ export interface Treatment {
   id: string;
   name: string;
   description: string;
-  duration_in_min: number;
+  available: boolean;
   price_in_euro: number;
   body_part: string;
 }
